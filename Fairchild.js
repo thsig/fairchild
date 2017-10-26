fa7ee5de97f01e8ea8bec35cd2a4231953f8ec6e
Thorarinn Sigurdsson <thorarinnsigurdsson@gmail.com>
2017-10-27 00:24:47 +0200

    Remove video compression logic.
    Video compression is now handled by react-native-video.

--- Fairchild.js
+++ Fairchild.js
@@ -12,7 +12,6 @@ var Fairchild = require('react-native').NativeModules.Fairchild;
 var NativeAppEventEmitter = require('react-native').NativeAppEventEmitter;  // iOS
 var Promise = require('bluebird');
 
-var _compressVideo = Promise.promisify(Fairchild.compressVideo);
 var _thumbForVideo = Promise.promisify(Fairchild.thumbForVideo);
 var _compressImage = Promise.promisify(Fairchild.compressImage);
 
@@ -22,53 +21,6 @@ var compressionError = (err) => {
 };
 
 var Fairchild = {
-
-  // Supported outputOtions:
-  // keepOriginal: boolean (default: false)
-  // fileType:         'mov' | 'mp4' (default: same as source)
-  // isAsset:          boolean (default: inferred from path prefix)
-  // resolution:       '1080p' | '720p' (default) | '480p'
-  // cropSquare:       boolean (default: false)
-  // cropSquareVerticalOffset : number from 0.0 to 1.0 (default: 0.0)
-  // bitRate:          integer (default: same as source)
-  // orientation:      'portrait' (default) | 'landscape'
-  // startTimeSeconds: float
-  // endTimeSeconds:   float
-  compressVideo(inputFilePath, outputOptions = {}) {
-    if (inputFilePath) {
-      if (inputFilePath.match(/^\//)) {
-        inputFilePath = `file://${inputFilePath}`;
-      }
-      var o = outputOptions;
-      var ft = o.fileType;
-      if (!o.fileType) { ft = extractFileType(inputFilePath); }
-      var isAsset = o.isAsset;
-      if (isAsset === null || isAsset === undefined) {
-        isAsset = !!inputFilePath.match(/^(assets-library|file):/);
-      }
-      var vo = o.cropSquareVerticalOffset || 0;
-      if (vo) {
-        if (vo > 1.0) { vo = 1.0; }
-        if (vo < 0.0) { vo = 0.0; }
-      }
-      var opts = {
-        fileType:                 ft,
-        keepOriginal:             o.keepOriginal || false,
-        isAsset:                  isAsset || false,
-        resolution:               o.resolution,
-        cropSquare:               o.cropSquare || false,
-        cropSquareVerticalOffset: vo,
-        bitRate:                  o.bitRate,
-        orientation:              o.orientation || 'portrait',
-        startTimeSeconds:         o.startTimeSeconds ? o.startTimeSeconds : -1,
-        endTimeSeconds:           o.endTimeSeconds ? o.endTimeSeconds : -1
-      };
-      return _compressVideo(inputFilePath, opts)
-        .catch(compressionError);
-    } else {
-      console.error('Error: Fairchild.compressVideo called with blank inputFilePath.');
-    }
-  },
 
   // Currently always returns a square thumb, starting from the top left.
   //
@@ -120,16 +72,5 @@ var Fairchild = {
   }
 
 };
-
-var extractFileType = (inputFilePath) => {
-  var segments = inputFilePath.split('.');
-  var ft = segments[segments.length - 1].toLowerCase();
-  if (ft === 'mp4' || ft === 'mov') {
-    return ft;
-  } else {
-    console.error('Error: Unsupported extension', ft, 'and no fileType provided in outputOptions.');
-    return null;
-  }
-}
 
 module.exports = Fairchild;
